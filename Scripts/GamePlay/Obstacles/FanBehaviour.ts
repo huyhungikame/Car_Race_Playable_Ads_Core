c262cc77f9c31b502d916ca3f66a83baaf45e080
huyhungikame <115602357+huyhungikame@users.noreply.github.com>
2024-04-03 15:40:04 +0700

    FanBehaviour

--- Scripts/GamePlay/Obstacles/FanBehaviour.ts
+++ Scripts/GamePlay/Obstacles/FanBehaviour.ts
@@ -1,21 +1,25 @@
-import { _decorator, CCFloat, Collider, Component, ICollisionEvent, Node, Vec3 } from 'cc';
+import { _decorator, Collider, Component, ICollisionEvent, Vec3 } from 'cc';
 import { CarCollider } from '../CarCollider';
 
 const { ccclass, property } = _decorator;
 
 @ccclass('FanBehaviour')
 export class FanBehaviour extends Component {
-    @property
-    speed: number = 0.0;
+    @property(Vec3)
+    private speedValue: Vec3 = new Vec3(0, 50, 0);
 
     @property({ type: Collider})
-    collider: Collider[] = [];
+    private collider: Collider[] = [];
+
+    private eulerAngles: Vec3 = new Vec3();
+    private dtSpeed: Vec3 = new Vec3();
 
     protected start(): void {
         for (let i = 0; i < this.collider.length; i++) { 
             this.collider[i].on('onCollisionEnter', this.onCollision, this);
         }
-        this.rotateZ =this.node.eulerAngles.z
+
+        this.eulerAngles = this.node.eulerAngles;
     }
 
     private onCollision (event: ICollisionEvent) {
@@ -23,12 +27,9 @@ export class FanBehaviour extends Component {
         player.onHitObstacles();
     }
 
-    rotateZ : number = 0;
     protected update(dt: number): void {
-        this.rotateZ += dt * this.speed ;
-        //if(this.rotateZ > 180) this.rotateZ = 0;
-        // var angle = new Vec3(this.node.eulerAngles);
-        // angle.add(new Vec3(0,0,dt * this.speed));
-        this.node.setRotationFromEuler(0, this.rotateZ,0);
+        this.dtSpeed.set(this.speedValue);
+        this.eulerAngles.add(this.dtSpeed.multiplyScalar(dt));
+        this.node.setRotationFromEuler(this.eulerAngles);
     }
 }
