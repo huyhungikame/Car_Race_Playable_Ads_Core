75ee13d5ad073158db9511ddfa8e3317a8a282b2
huyhungikame <115602357+huyhungikame@users.noreply.github.com>
2024-04-03 15:56:03 +0700

    Update SawMovement.ts

--- Scripts/GamePlay/Obstacles/SawMovement.ts
+++ Scripts/GamePlay/Obstacles/SawMovement.ts
@@ -25,11 +25,19 @@ export class SawMovement extends Component {
     @property(Node)
     graphic: Node;
 
+    @property(Node)
+    effect: Node;
+
     @property(Vec3)
     rotaion: Vec3 = new Vec3(0,0,1);
 
+    private controlEffect: boolean = false;
+    private currentRotation: Vec3 = new Vec3();
+    private currentPosition: Vec3 = new Vec3();
+
     protected start(): void {
         this.collider.on('onCollisionEnter', this.onCollision, this);
+        this.controlEffect = this.effect != null;
     }
 
     private onCollision (event: ICollisionEvent) {
@@ -44,20 +52,21 @@ export class SawMovement extends Component {
     update(deltaTime: number) {
         var angle = this.graphic.eulerAngles;
         var offsetRoation = -1 * this.startDirection * 360 * deltaTime;
-        angle.add(new Vec3(this.rotaion).multiplyScalar(offsetRoation));
+        angle.add(this.currentRotation.set(this.rotaion).multiplyScalar(offsetRoation));
         this.graphic.eulerAngles = angle;
         
         var newOffset = this.currentOffset + this.startDirection * this.speed * deltaTime;
-        this.currentOffset = lerp(this.currentOffset,newOffset,0.7);
+        this.currentOffset = lerp(this.currentOffset, newOffset, 0.7);
 
         if(Math.abs(this.currentOffset) >= this.lenght) {
             this.currentOffset = this.startDirection * this.lenght;
             this.startDirection *= -1;
         }
 
-        var newPos = new Vec3(this.graphic.position);
-        newPos.x = this.originPosX + this.currentOffset;
-        this.graphic.setPosition(newPos);
+        this.graphic.getPosition(this.currentPosition);
+        this.currentPosition.x = this.originPosX + this.currentOffset;
+        this.graphic.setPosition(this.currentPosition);
+        if(this.controlEffect) this.effect.setPosition(this.currentPosition);
     }
 }
 
